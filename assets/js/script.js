1eef77e4d985a95392734695d61f05db16fa6bd1
Zach Smith <Lemelisk27@gmail.com>
2021-10-01 10:10:04 -0700

    updated code

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// var APIKey = "e5dea1d1c3f38ea6d30bc512279c3257"
+// var city = "seattle"
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+
+// fetch(queryURL)
+//     .then(function (response) {
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data)
+//     })
